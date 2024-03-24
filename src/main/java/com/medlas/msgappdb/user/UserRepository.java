8561f19ec867dc4059c485fc85a52cec6cbd2895
med-las <lassoued.mohamed@esprit.tn>
2024-03-24 01:01:15 +0100

    messaging app-works

---
+++ src/main/java/com/medlas/msgappdb/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.medlas.msgappdb.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    List<User> findByStatus(Status status);
+
+}
